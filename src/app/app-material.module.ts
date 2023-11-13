472fa8774f1f21c654871d2ac455e48ee5f6700e
Yuubitsume <jeannicolas34980@gmail.com>
2023-11-13 12:16:50 +0100

    add all imports

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { LayoutModule } from '@angular/cdk/layout';
 
 
 @NgModule({
   exports: [
-    MatTableModule
-
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
   ]
 })
 export class AppMaterialModule { }
